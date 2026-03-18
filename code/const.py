203b3c5ddd95756ee5532c3c6aa6407e2fd8cedd
RuhamaPJ <ruhcas@live.com>
2026-03-17 21:44:34 -0300

    Creating the menu: background, sound and fonts

---
+++ code/const.py
@@ -0,0 +1,18 @@
+#contante tem que ser definida com letra maiúscula
+
+#C
+COLOR_GREEN = (87, 91, 40)
+COLOR_WHITE = (255, 255, 255)
+
+#M
+MENU_OPTION = ('NEW GAME 1P',
+               'NEW GAME 2P - COOPERATIVE',
+               'NEW GAME 2P - COMPETITIVE',
+               'SCORE',
+               'EXIT')
+
+
+
+#W
+WIN_WIDTH = 576
+WIN_HEIGHT = 324
